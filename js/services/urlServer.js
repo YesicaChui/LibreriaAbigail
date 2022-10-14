89bdf34359643dd675928b72acf08d1d0ec51bc5
YesicaChui <yesicachuic@gmail.com>
2022-10-14 12:53:14 -0500

    configurando ruta desplegada del backend

--- js/services/urlServer.js
+++ js/services/urlServer.js
@@ -1,2 +1,2 @@
-const API_URL = "https://3903-2800-200-f580-769-14a7-ee6c-d97e-b94b.sa.ngrok.io";
+const API_URL = "https://yesica-chui-ecommerce.herokuapp.com";
 const API_URL2 = "http://127.0.0.1:5000";
